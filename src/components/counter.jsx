e3f90eb9a3a019d3cc96acd2efcf21469d3ad123
qiannyqian <liang.huiqian@gmail.com>
2020-05-08 10:52:54 +0800

    updated counter app

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,51 +1,37 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ['tag1', 'tag2', 'tag3'],
-  };
-
   // use arrow function syntax to bind event handlers
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags.</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
 
   getBadgeClasses() {
-    let classes = 'badge m-2  badge-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
+    let classes = 'badge m-2 badge-';
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    const { value } = this.props.counter;
+    return value === 0 ? 'Zero' : value;
   }
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-small"
         >
           Increment
         </button>
 
-        {this.renderTags()}
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
